dbcaa990b060b36a9fc8c60ca36f46fcd79d1974
David Lechner <david@lechnology.com>
2021-11-20 15:21:28 -0600

    add command line option for static content
    This adds a command line option for specifiying the location of the web
    client static files to be served.

--- src/skywrangler_web_server/__main__.py
+++ src/skywrangler_web_server/__main__.py
@@ -1,8 +1,20 @@
+import argparse
 import logging
+import pathlib
 
 from .main import main
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--web-client-path",
+        metavar="<directory>",
+        type=pathlib.Path,
+        help="path to the web client directory",
+    )
+
+    args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    main()
+    main(args.web_client_path)
--- src/skywrangler_web_server/main.py
+++ src/skywrangler_web_server/main.py
@@ -1,12 +1,27 @@
+from typing import Optional
 from aiohttp import web
+from aiohttp.typedefs import PathLike
 
 from .api import routes
 from .rpi import RPi
 
 
-def main() -> None:
+async def root_handler(request):
+    # redirect '/' to index.html
+    return web.HTTPFound("/index.html")
+
+
+def main(static_path: Optional[PathLike] = None) -> None:
     app = web.Application()
+
     app.router.add_routes(routes)
+
+    # If a path is given, serve static content, e.g the web client.
+    # NB: since this uses the root path, it needs to go after all other routes.
+    if static_path:
+        app.router.add_route("*", "/", root_handler)
+        app.router.add_static("/", static_path)
+
     app["rpi"] = RPi()
 
     web.run_app(app)
