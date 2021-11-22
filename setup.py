3e4c501974b2e320003203a79dbc82706ffffc8b
David Lechner <david@lechnology.com>
2021-11-22 03:59:29 +0000

    add debian packaging
    - had to switch from poetry to setup.py
    - added devcontainer

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="skywrangler-web-server",
+    version="1.0.0",
+    author="David Lechner",
+    author_email="david@ablerobots.com",
+    description="Sky Wrangler onboard computer web server",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=["aiohttp"],
+    entry_points={
+        "console_scripts": [
+            "skywrangler-web-server = skywrangler_web_server.__main__:main"
+        ]
+    },
+)
