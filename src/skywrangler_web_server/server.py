910176ff9545b0c2047159d5e23b938635f37c69
David Lechner <david@lechnology.com>
2021-11-22 20:10:26 +0000

    fix systemd nginx interop
    - drop the .socket unit file - this didn't work as expected
    - fix up the .service unit file
    - add systemd notify support
    
    The service was attemting to start multiple times because it was enabled
    by default and the socket also triggered it. The systemd socket also
    caused an error since it used the same address/port. Also nginx would
    fail to start because the aiohttp server wasn't ready soon enough, so
    using systemd notify lets us delay nginx starting until we are ready.

--- src/skywrangler_web_server/server.py
+++ src/skywrangler_web_server/server.py
@@ -1,9 +1,21 @@
+import asyncio
 from typing import Optional
+
 from aiohttp import web
 from aiohttp.typedefs import PathLike
+from sdnotify import SystemdNotifier
 
 from .api import routes
 from .rpi import RPi
+
+
+async def on_startup(app: web.Application) -> None:
+    # FIXME: how to wait for socket to become ready?
+    # Without this delay, nginx will fail to start up because it can't see the
+    # server. It would be better if we could find the actual condition to test
+    # for instead of using an arbitrary delay.
+    await asyncio.sleep(10)
+    SystemdNotifier().notify("READY=1")
 
 
 async def root_handler(request):
@@ -24,6 +36,7 @@ def serve(port: Optional[int] = None, static_path: Optional[PathLike] = None) ->
     app = web.Application()
 
     app.router.add_routes(routes)
+    app.on_startup.append(on_startup)
 
     # If a path is given, serve static content, e.g the web client.
     # NB: since this uses the root path, it needs to go after all other routes.
