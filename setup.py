0f5fc3da0a182457270ef4ca5d8b36f159ac67c7
David Lechner <david@ablerobots.com>
2021-11-23 04:03:51 +0000

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="skywrangler-web-server",
-    version="1.0.0",
+    version="1.1.0",
     author="David Lechner",
     author_email="david@ablerobots.com",
     description="Sky Wrangler onboard computer web server",
