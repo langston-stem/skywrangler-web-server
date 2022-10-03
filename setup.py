8b6a9777c57dd1bfe7b5a542a42544d1b39b6766
David Lechner <david@ablerobots.com>
2022-10-03 17:02:21 +0000

    update for 1.5.0 release

--- setup.py
+++ setup.py
@@ -2,14 +2,22 @@ import setuptools
 
 setuptools.setup(
     name="skywrangler-web-server",
-    version="1.4.4",
+    version="1.5.0",
     author="David Lechner",
     author_email="david@ablerobots.com",
     description="Sky Wrangler onboard computer web server",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.9",
-    install_requires=["aiohttp", "aiohttp-sse", "dbus-next", "mavsdk", "sdnotify"],
+    install_requires=[
+        "aiohttp-sse",
+        "aiohttp",
+        "dbus-next",
+        "mavsdk",
+        "pyproj",
+        "rx",
+        "sdnotify",
+    ],
     entry_points={
         "console_scripts": [
             "skywrangler-web-server = skywrangler_web_server.__main__:main"
