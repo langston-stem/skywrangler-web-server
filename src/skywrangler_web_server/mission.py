96b0b4cb45176e9221f526f04f733ed821a7d3f4
junetheprogrammer <juniorotuonye70@gmail.com>
2022-11-08 16:32:02 -0600

    Revert "Merge branch 'Transect-Points' of https://github.com/langston-stem/skywrangler-web-server into Transect-Points"
    This reverts commit 0a0450694887fe3906f490f1c29983c55c7bf869, reversing
    changes made to 22ba6a544b1cb54c4f0291c9f614468f55eb149c.

--- src/skywrangler_web_server/mission.py
+++ src/skywrangler_web_server/mission.py
@@ -42,33 +42,17 @@ class Parameters(NamedTuple):
 def transect_points(
     origin: Origin, transect: Transect, parameters: Parameters
 ) -> Tuple[Point, Point]:
-    """
-    Calculates transect start and end points based on the experiment parameters.
-
-    Args:
-        origin: The center of the goat enclosure.
-        transect: The path of the drone as it passes by the enclosure.
-        paramters: The variable parameters of the experiment.
-
-    Returns:
-        A tuple of the starting and ending transect points.
-    """
 
     horizontal, vertical = dist_ang_to_horiz_vert(parameters.distance, parameters.angle)
-
-    # m is the midpoint of the transect
     m_lat, m_lon = relative_point(
         origin.latitude, origin.longitude, horizontal, transect.azimuth
     )
-    # b is the starting point of the transect
     b_lat, b_lon = relative_point(
         m_lat, m_lon, transect.length / 2, transect.azimuth - 90
     )
-    # c is the ending point of the transect
     c_lat, c_lon = relative_point(
         m_lat, m_lon, transect.length / 2, transect.azimuth + 90
     )
-
     return (
         Point(b_lat, b_lon, origin.elevation + vertical),
         Point(c_lat, c_lon, origin.elevation + vertical),
