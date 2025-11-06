612fe83e5e29e7bcc1efa9351efb67684807a4b7
David Lechner <david@lechnology.com>
2025-11-06 17:12:43 -0600

    main: add json file for parameters and loop to create multiple missions

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 import json
+from typing import TypedDict
 from qgc_mission import QgcJSONEncoder
 from sw_mission import create_mission
 from sw_mission.points import Coordinate2D, Parameters, Point, Transect
 
 
-if __name__ == "__main__":
-    plan = create_mission(
-        launch=Point(
-            latitude=35.9301904295499, longitude=-97.26450295241108, altitude=307
-        ),
-        origin=Point(
-            latitude=35.932121645130756, longitude=-97.2631249266781, altitude=304
-        ),
-        transect=Transect(azimuth=-85, length=100),
-        parameters=Parameters(speed=5, angle=90, distance=30),
-        return_point=Coordinate2D(
-            latitude=35.934456813161006, longitude=-97.2646272318608
-        ),
-    )
+class MissionData(TypedDict):
+    home: dict[str, float]
+    origin: dict[str, float]
+    transect: dict[str, float]
+    away: dict[str, float]
+    variables: dict[str, list[float]]
 
-    with open("test.plan", "w") as f:
-        json.dump(plan, f, cls=QgcJSONEncoder, indent=4)
+
+if __name__ == "__main__":
+    with open("mission-data.json", "r") as f:
+        mission_data = json.load(f)
+
+    for speed in mission_data["variables"]["speed"]:
+        for angle in mission_data["variables"]["angle"]:
+            for distance in mission_data["variables"]["distance"]:
+
+                plan = create_mission(
+                    launch=Point(
+                        latitude=mission_data["home"]["latitude"],
+                        longitude=mission_data["home"]["longitude"],
+                        altitude=mission_data["home"]["altitude"],
+                    ),
+                    origin=Point(
+                        latitude=mission_data["origin"]["latitude"],
+                        longitude=mission_data["origin"]["longitude"],
+                        altitude=mission_data["origin"]["altitude"],
+                    ),
+                    transect=Transect(
+                        azimuth=mission_data["transect"]["azimuth"],
+                        length=mission_data["transect"]["length"],
+                    ),
+                    parameters=Parameters(speed=speed, angle=angle, distance=distance),
+                    return_point=Coordinate2D(
+                        latitude=mission_data["away"]["latitude"],
+                        longitude=mission_data["away"]["longitude"],
+                    ),
+                )
+
+                with open(f"skywrangler_s{speed}_a{angle}_d{distance}.plan", "w") as f:
+                    json.dump(plan, f, cls=QgcJSONEncoder, indent=4)
